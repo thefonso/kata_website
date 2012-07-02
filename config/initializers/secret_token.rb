9ed30c79aec9faf36594fb7b92da5a49554c0d4b
Alfonso <alephmage@gmail.com>
2012-07-02 12:47:07 -0500

    let's begin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Formkata::Application.config.secret_token = '3baf1e90e7681ed4451d79291e947276e7bb9e8499503ed299aa0aafb015c2afc6c29003bb39cf43e6b3d19ce4ed5ebcb671c5ccd95d068cc1b90b4a1b555d86'
