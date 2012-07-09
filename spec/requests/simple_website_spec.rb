34628a5a21cdf7f9258186f59b721bceea8c2c9c
Alfonso <alephmage@gmail.com>
2012-07-09 13:45:05 -0500

    here is a form

--- spec/requests/simple_website_spec.rb
+++ spec/requests/simple_website_spec.rb
@@ -19,9 +19,23 @@ describe "SimpleWebsite" do
         get simple_website_contact_path
         response.status.should be(200)
       end
-      xit "should contain a form" do
-        get 'simple_website/contact'
-        response.should have_selector("form")
+      describe "form" do
+        it "should contain a form tag" do
+          get 'simple_website/contact'
+          response.should have_selector("form")
+        end
+        it "should contain a name field, an email field and a comment field" do
+          get 'simple_website/contact'
+          response.should have_selector("input", :name => "firstname")
+        end
+        it "should contain an email field" do
+          get 'simple_website/contact'
+          response.should have_selector("input", :name => "email")
+        end
+        it "should contain and a comment field" do
+          get 'simple_website/contact'
+          response.should have_selector("textarea", :name => "comment")
+        end
       end
     end
   end
