c42dc6d54e02998d079ff5dbeb0727465c03b3d4
alfonso rush <alephmage@gmail.com>
2012-07-17 14:04:24 -0500

    rspec test updated for new 2.11.0

--- Gemfile
+++ Gemfile
@@ -2,8 +2,8 @@ source 'https://rubygems.org'
 
 # adding gems for starting RSpec and Cucumber fun
 group :development do
-  gem "rspec-rails", ">= 2.0.0" 
-  gem "webrat", ">= 0.7.2"
+  gem "rspec-rails", ">= 2.10.0" 
+  gem "webrat", ">= 0.7.2" 
 end
 group :test do
   gem "cucumber-rails", "~> 1.0", require: false #use this or get goofy error
@@ -14,35 +14,14 @@ gem 'sqlite3-ruby', :require => 'sqlite3'
 
 gem 'rails', '3.2.3'
 
-# commenting out this one for now
-# gem 'sqlite3'
-
 
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
-
-  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platform => :ruby
-
   gem 'uglifier', '>= 1.0.3'
 end
 
 gem 'jquery-rails'
 
-# To use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
-
-# To use Jbuilder templates for JSON
-# gem 'jbuilder'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Deploy with Capistrano
-# gem 'capistrano'
-
-# To use debugger
-# gem 'ruby-debug19', :require => 'ruby-debug'
--- spec/requests/simple_website_spec.rb
+++ spec/requests/simple_website_spec.rb
@@ -1,42 +1,36 @@
 require 'spec_helper'
 
 describe "SimpleWebsite" do  
-  it "should have three pages, home, about, contact" do
-    describe "Home page" do
-      it "should exist" do
+  context "should have three pages, home, about, contact" do
+    it "should have a home page" do
         get simple_website_home_path
         response.status.should be(200)
-      end
     end
-    describe "about page" do
-      it "should exist" do
-        get simple_website_about_path
-        response.status.should be(200)
-      end
+    it "should have an about page" do
+      get simple_website_about_path
+      response.status.should be(200)
     end
-    describe "contact page" do
-      it "should exist" do
-        get simple_website_contact_path
-        response.status.should be(200)
-      end
-      describe "form fields" do
-        it "should contain a form tag" do
-          get 'simple_website/contact'
-          response.should have_selector("form")
-        end       
-        xit "should contain a name field, an email field and a comment field" do
-          get 'simple_website/contact'
-          response.should have_selector("input", :name => "firstname")
-        end
-        xit "should contain an email field" do
-          get 'simple_website/contact'
-          response.should have_selector("input", :name => "email")
-        end
-        xit "should contain and a comment field" do
-          get 'simple_website/contact'
-          response.should have_selector("textarea", :name => "comment")
-        end
-      end
+    it "should have a contact page" do
+      get simple_website_contact_path
+      response.status.should be(200)
+    end
+  end
+  context "contact page form fields" do
+    it "should contain a form tag" do
+      get 'simple_website/contact'
+      response.should have_selector("form")
+    end       
+    xit "should contain a name field" do
+      get 'simple_website/contact'
+      response.should have_selector("input", :name => "firstname")
+    end
+    xit "should contain an email field" do
+      get 'simple_website/contact'
+      response.should have_selector("input", :name => "email")
+    end
+    xit "should contain and a comment field" do
+      get 'simple_website/contact'
+      response.should have_selector("textarea", :name => "comment")
     end
   end
 end
